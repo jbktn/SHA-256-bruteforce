146a2cbc87ad6f0cdbe82b3568ad11725359949f
Jakub Pomorski <80984903+jbktn@users.noreply.github.com>
2024-06-09 17:11:01 +0200

    Update and rename hack2.py to SHA256bruteforce.py

--- SHA256bruteforce.py
+++ SHA256bruteforce.py
@@ -17,6 +17,10 @@ digits = [
     *[str(i) for i in range(0, 10)]
 ]
 
+lowercase = [
+    *[chr(i) for i in range(ord('a'), ord('z') + 1)]
+]
+
 alpha = [
     *[chr(i) for i in range(ord('a'), ord('z') + 1)],
     *[chr(i) for i in range(ord('A'), ord('Z') + 1)]
@@ -33,42 +37,44 @@ charset_extended = [
     *[chr(i) for i in range(ord('a'), ord('z') + 1)],
     *[chr(i) for i in range(ord('A'), ord('Z') + 1)],
     "-", "_", "+", "!", "@", "#", "$", "%", "^", "&", "*", "=", "!", "[", "]", "{", "}", "/", "?", ".", ",", "<", ">",
-    ":", ";", "'", " ", "|"
-]
+    ":", ";", "'", " ", "|"]
 
 
 def brute_force(charset, password, repeat):
     shuffle(charset)
     length = 1
-    counter = 0
+    #counter = 0
     t1 = time.perf_counter()
     for x in range(repeat ** length):
-        idx = 1
+        #idx = 1
         for generated in product(charset, repeat=length):
             gen = ''.join(generated)
             g = hashlib.sha256(gen.encode("utf-8")).hexdigest()
-            print(f"{idx:,} | generated: {gen} |  {'SUCCESS' if g == password else 'FAIL'}")
+            #print(f"{idx:,} | generated: {gen} |  {'SUCCESS' if g == password else 'FAIL'}")
             if g == password:
                 break
-            idx += 1
+            #idx += 1
         else:
-            counter += idx
+            #counter += idx
             length += 1
             continue
         break
     t2 = time.perf_counter()
     print(f"{bcolors.PASSWD}password: {gen} {bcolors.STOCK} | hashed: {g}")
-    print(f"c/s: {floor((counter - 1) / (t2 - t1))} | time: {round((t2 - t1), 5)}s | length: {length}")
+    #print(f"c/s: {floor((counter - 1) / (t2 - t1))} | time: {round((t2 - t1), 5)}s | length: {length}")
+    print(f"time: {round((t2 - t1), 5)}s | length: {length}")
 
 
 if __name__ == "__main__":
     passwd = input("Enter hash: ")
-    mode = int(input("1 - digits\n2 - alpha\n3 - simple\n4 - extended\n"))
+    mode = int(input("1 - digits\n2 - lowercase\n3 - alpha\n4 - simple\n5 - extended\n"))
     if mode == 1:
         brute_force(digits, passwd, 10)
     elif mode == 2:
-        brute_force(alpha, passwd, 52)
+        brute_force(alpha, passwd, 26)
     elif mode == 3:
-        brute_force(charset_simple, passwd, 62)
+        brute_force(alpha, passwd, 52)
     elif mode == 4:
+        brute_force(charset_simple, passwd, 62)
+    elif mode == 5:
         brute_force(charset_extended, passwd, 92)
